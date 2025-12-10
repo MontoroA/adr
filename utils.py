2cebe28af3b716aa27869e7545cadf3ad8652ad6
montoroA <andresmontoro01@gmail.com>
2025-12-10 12:10:03 -0300

    unify siege/transaction logic

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 
+from abc import ABC, abstractmethod
 from math import floor
 from pydistsim.algorithm.node_wrapper import NodeAccess
 from pydistsim.message import Message
@@ -16,10 +17,6 @@ def raiseError(message: str):
     raise Exception(message)
 
 
-######################################################################################################################################################
-#################################################        ORAL MESSAGES  UTILS             ############################################################
-######################################################################################################################################################
-
 class Siege():
     class State(Enum):
         ONGOING = "ongoing"
@@ -27,13 +24,17 @@ class Siege():
         FAILED = "failed"
         ABORTED = "aborted"
 
-    def __init__(self, n, success_threshold = None, success_rate = 0.5):
+        def __str__(self):
+            return self.value
+
+    def __init__(self, n, logging, success_threshold = None, success_rate = 0.5):
         self.siegers = n
         self.attackers = 0
         self.retreaters = 0
         self.state = self.State.ONGOING
         self.success_threshold = success_threshold if success_threshold else n
         self.success_rate = success_rate
+        self.logging = logging
 
     def observe(self, node: NodeAccess):
         d = random.random()
@@ -44,27 +45,29 @@ class Siege():
 
     def attack_in_place(self):
         if self.attackers >= self.success_threshold:
-            logger.info("[{}] All loyal generals attacked. Successful siege", "Siege")
+            self.logging.append("[Siege] All loyal generals attacked. Successful siege")
             self.state = self.State.SUCCESSFUL
         elif self.attackers == 0:
-            logger.info("[{}] All loyal generals retreat from siege", "Siege")
+            self.logging.append("[Siege] All loyal generals retreat from siege")
             self.state = self.State.ABORTED
         else:
-            logger.info("[{}] Only some generals attacked. Failed siege", "Siege")
+            self.logging.append("[Siege] Only some generals attacked. Failed siege")
             self.state = self.State.FAILED
 
-    def attack(self):
-        self.siegers -= 1
+    def attack(self, node: NodeAccess = None):
+        self.logging.append(f"[{node.memory['unique_value']}] Attacking")
         self.attackers += 1
-        if self.siegers == 0:
+        if self.attackers + self.retreaters == self.siegers:
             self.attack_in_place()
 
-    def retreat(self):
-        self.siegers -= 1
+    def retreat(self, node: NodeAccess = None):
+        self.logging.append(f"[General {node.memory['unique_value']}] Retreating")
         self.retreaters += 1
-        if self.siegers == 0:
+        if self.attackers + self.retreaters == self.siegers:
             self.attack_in_place()
 
+    def crash(self):
+        self.siegers -= 1
 
 
 class GeneralDecision(Enum):
@@ -116,9 +119,31 @@ def send_with_check(
 
 
 
-class TraitorActions:
-    @staticmethod
-    def send_confusing_signal(general : NodeAccess, algorithm, path : str, liutenants : set):
+class FaultyBehavior(ABC):
+    @abstractmethod
+    def send(*args, **kwargs):
+        pass
+
+    
+
+class ByzantineBehavior(FaultyBehavior):
+    class Behavior(Enum):
+        LIER = "lier"
+        QUIET = "quiet"
+        CONFUSER = "confuser"
+
+    def __init__(self, behavior : Behavior):
+        self.behavior = behavior
+
+    def send(self, general : NodeAccess, algorithm, path : str, liutenants : set, rcvd_decision : GeneralDecision):
+        if self.behavior == ByzantineBehavior.Behavior.LIER:
+            self.lie(general, algorithm, path, liutenants, rcvd_decision)
+        elif self.behavior == ByzantineBehavior.Behavior.QUIET:
+            self.quiet()
+        elif self.behavior == ByzantineBehavior.Behavior.CONFUSER:
+            self.confuse(general, algorithm, path, liutenants)
+
+    def confuse(self, general : NodeAccess, algorithm, path : str, liutenants : set):
         half = int(len(liutenants)/2)
         attackers = random.sample(list(liutenants), half)
         retreaters = [x for x in liutenants if x not in attackers]     
@@ -131,8 +156,6 @@ class TraitorActions:
             path = path,
             value = GeneralDecision.RETREAT
         )
-
-
         send_with_check(
             general, 
             datos=datosAttackers,
@@ -148,72 +171,53 @@ class TraitorActions:
             algorithm=algorithm
         )
 
-    @staticmethod
-    def lie(decision):
-        if decision is GeneralDecision.RETREAT:
-            return GeneralDecision.ATTACK
-        if decision is GeneralDecision.ATTACK:
-            return GeneralDecision.RETREAT
-        return None
-    
+    def lie(self, general : NodeAccess, algorithm, path : str, liutenants : set, rcvd_decision : GeneralDecision):
+        if rcvd_decision is GeneralDecision.RETREAT:
+            lie = GeneralDecision.ATTACK
+        if rcvd_decision is GeneralDecision.ATTACK:
+            lie = GeneralDecision.RETREAT
+        bromita = Data(
+            path = path,
+            value = lie
+        )
+        send_with_check(
+            general, 
+            datos=bromita,
+            dest=liutenants,
+            msj_type=algorithm.default_params["Value"],
+            algorithm=algorithm
+        )
+        
+    def quiet():
+        pass
 
+class CrashBehavior(FaultyBehavior):
+    def __init__(self, node : NodeAccess, logging):
+        self.crash_chance = self.crash_chance()
+        self.logging = logging
 
+    def send(self, node : NodeAccess, algorithm, header, to : set, chance = False):
+        if not isinstance(to, set):
+            to = {to}
+        for neighbor in to:
+            if(chance and self.determine_crash(node)):
+                return False
+            self.logging.append(f"[Node {node.memory['unique_value']}] Sending '{header}' through label {neighbor}")
+            algorithm.send(
+                node, 
+                data=None,
+                destination=neighbor,
+                header=header
+            )
+        return True
 
-######################################################################################################################################################
-#################################################               2PC  UTILS                ############################################################
-######################################################################################################################################################
-
-class CrashBehavior():
-    @staticmethod
     def crash_chance(node: NodeAccess):
         # TODO fallos configurables
         # return random.random()
         return 0.1
 
-    @staticmethod
-    def determine_crash(node: NodeAccess):
+    def determine_crash(self, node: NodeAccess):
         d = random.random()
-        if d < node.memory["crash_chance"]:
+        if d < self.crash_chance:
             return True
         return False
-
-class Transaction():
-    class TransactionState(Enum):
-        ONGOING = "ongoing"
-        COMMITTED = "committed"
-        ABORTED = "aborted"
-        INCONSISTENT = "inconsistent"
-
-    def __init__(self, n):
-        self.participants = n
-        self.committers = 0
-        self.aborters = 0
-        self.state = self.TransactionState.ONGOING
-
-    def result(self):
-        if self.committers == self.participants:
-            logger.info("[Transaction] All nodes committed the transaction. Consistency achieved")
-            self.state = self.TransactionState.COMMITTED
-        elif self.aborters == self.participants:
-            logger.info("[Transaction] All nodes aborted the transaction. Consistency preserved")
-            self.state = self.TransactionState.ABORTED
-        else:
-            logger.info("[Transaction] Only some nodes commited the transaction. Inconsistency occurred")
-            self.state = self.TransactionState.INCONSISTENT
-
-    def commit(self):
-        if self.state != self.TransactionState.ONGOING:
-            return
-        self.committers += 1
-        if self.committers + self.aborters == self.participants:
-            self.result()
-
-    def abort(self):
-        if self.state != self.TransactionState.ONGOING:
-            return
-        self.aborters += 1
-        if self.committers + self.aborters == self.participants:
-            self.result()
-
-    def crash(self):
-        self.participants -= 1
